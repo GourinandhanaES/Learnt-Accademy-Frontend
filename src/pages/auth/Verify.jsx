caa4f81ed2a4921449261b9dcc1403cb933aac33
GourinandhanaES <gourinandhana028@gmail.com>
2024-12-28 23:46:18 +0530

    frontend edit

--- src/pages/auth/Verify.jsx
+++ src/pages/auth/Verify.jsx
@@ -19,29 +19,32 @@ const Verify = () => {
     await verifyOtp(Number(otp), navigate);
   };
   return (
-    <div className="auth-page">
-      <div className="auth-form">
-        <h2>Verify Account</h2>
+    <div className="auth-page d-flex justify-content-center align-items-center">
+      <div className="auth-form p-4 shadow-lg rounded bg-white" style={{ maxWidth: "400px", width: "100%" }}>
+        <h2 className="head text-center mb-4">Verify Account</h2>
         <form onSubmit={submitHandler}>
-          <label htmlFor="otp">Otp</label>
+          <div className="text-center mb-3">
+          <label htmlFor="otp" className="me-2">Otp</label>
           <input
             type="number"
             value={otp}
             onChange={(e) => setOtp(e.target.value)}
             required
           />
+          </div>
+          <div className="d-flex justify-content-center align-items-center text-center mb-3">
           <ReCAPTCHA
             sitekey=" 6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
             onChange={onChange}
           />
-          ,
+          </div>
           {show && (
             <button disabled={btnLoading} type="submit" className="common-btn">
               {btnLoading ? "Please Wait..." : "Verify"}
             </button>
           )}
         </form>
-        <p>
+        <p className="text-center mt-3">
           Go to <Link to="/login">Login</Link> page
         </p>
       </div>
